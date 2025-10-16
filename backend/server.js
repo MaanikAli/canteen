cbb1bee608ba5bbbf163c4c4a96f514d4cf63218
Temp <temp@example.com>
2025-10-17 02:38:07 +0600

    feat: update CORS configuration to allow requests from Vercel deployment and add .vercel to .gitignore

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://canteen-three.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
